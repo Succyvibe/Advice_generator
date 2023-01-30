cecf98128807fb40282ff7f2b6d469294066827c
success ibekwe <Successibekwe047@gmail.com>
2023-01-30 16:34:06 +0100

    fetched advice data and created the logic for getting random advice

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+const adviceId = document.getElementById("advice-id");
+const theAdvice = document.getElementById("the-advice");
+
+const adviceBtn = document.getElementById("advice-btn");
+adviceBtn.addEventListener("click", () => {
+  getAdvice();
+});
+
 const getAdvice = async () => {
-  const response = await fetch("https://api.adviceslip.com/advice");
-
-  if (response.status !== 200) {
-    throw new Error("cannot fetch the data");
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    adviceId.innerHTML = `#${data.slip.id}`;
+    theAdvice.innerHTML = `"${data.slip.advice}"`;
+  } catch (err) {
+    adviceId.innerHTML = "Error";
+    theAdvice.innerHTML = "Failed to load advice. Please try again";
+    console.log(err.message);
   }
-
-  const data = await response.json();
-
-  return data;
 };
 
-getAdvice()
-  .then((data) => console.log("resolved", data))
-  .catch((err) => console.log("rejected", err.message));
+getAdvice();
